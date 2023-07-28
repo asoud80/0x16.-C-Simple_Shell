07e72ea13a8dba18305866965eed93a4d866be3b
asoud80 <amgad_a.soud@outlook.com>
2023-07-28 14:23:24 -0700

    update

--- simple_builtin.c
+++ simple_builtin.c
@@ -25,7 +25,7 @@ int _myexit(info_t *info)
 			info->status = 2;
 			print_error(info, "illegal number: ");
 			_eputs(info->argv[1]);
-			_eputchar('\n');
+			_putchar('\n');
 			return (1);
 		}
 		info->err_num = _erratoi(info->argv[1]);
@@ -79,7 +79,7 @@ int _mycd(info_t *info)
 	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+		_eputs(info->argv[1]), _putchar('\n');
 	}
 	else
 	{
